b57ee69c804b41dd90e814a09092fc6b4e711a8d
M Iqbal Al Anshori <iqbaltelagaharapan@gmail.com>
2023-10-22 19:34:32 +0700

    menambahkan file latihan 1,2,3.py

---
+++ latihan1.py
@@ -0,0 +1,9 @@
+# Mengambil input
+nama = input("Siapa nama kamu:")
+umur = input("Berapa umur kamu:")
+
+# Menampilkan output
+print("Hello",nama,"umur kamu adalah",umur)
+
+print("hello")
+print("saya sedang belajar python")
